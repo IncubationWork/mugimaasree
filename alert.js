7751ddfed1ea87a7b68b15661e57e21e325d8b75
mugimaasree <mugimaasree@gmail.com>
2023-06-02 20:10:19 +0530

    arithmetic

---
+++ alert.js
@@ -0,0 +1,127 @@
+let na="mugi";
+let msg="welcome " + na + "  to ja";
+alert(msg);
+console.log(msg);
+ let number1=5;
+ let number2=45;
+ let result=number1+number2;
+ console.log(result);
+ let firstName="mugi";
+ let age=22;
+ let isYoung=true;
+ let lastName=undefined;
+ let tropy=null;
+ let naMe="mugimaasree";
+ let gender="female"
+ let adress="chennai,tamilnadu"
+ let person={naMe:"mugimaasree",
+  gender:"female",
+  adress:"chennai,tamilnadu",
+age:22,
+sibiling:{
+    brother:"suthan",
+    sister:"mugimaasree"
+}}
+ console.log(person);
+ let favColor=["red", "green" ,"blue"]
+ favColor[4]="yellow";
+ console.log(favColor);
+ function zohoUser(firstName,lastName){
+    let msg= "hello "+ firstName + " " + lastName +  " I am learning javascript!";
+    console.log(msg)
+ }
+ zohoUser("mugimaa","sree")
+ zohoUser("suthan","jeni")
+
+ function add(number1,number2){
+    console.log(number1+number2)
+ }
+ add(45,789)
+
+ function sub(number1,number2){
+    console.log(number1-number2)
+ }
+ sub(10,5)
+
+ function mul(number1,number2){
+    console.log(number1*number2)
+ }
+ mul(10,2)
+
+ function div(number1,number2){
+    console.log(number1/number2)
+ }
+div(10,2)
+
+let num=30;
+let num2=20;
+console.log(num+num2)
+console.log(num-num2)
+console.log(num*num2)
+console.log(num/num2)
+console.log(num%num2)
+
+let num3=5;
+console.log(++num3);
+console.log(num3);
+console.log(num3++);
+console.log(num3);
+
+let num4=4;
+console.log(--num4);
+console.log(num4);
+console.log(num4--);
+console.log(num4);
+
+let num5=6;
+document.write(1>6);
+document.write(1<6);
+document.write(num5<=6);
+document.write(num5>=7);
+document.write(num5===6);
+document.write(num5!==6);
+
+let x=5;
+x+=5;
+document.write(x);
+
+let x2=6;
+x2-=5;
+console.log(x2);
+
+let x3=6;
+x3*=5;
+console.log(x3);
+
+let x4=7;
+x4/=5;
+console.log(x4);
+
+let x5=5;
+x5%=5;
+console.log(x5);
+
+console.log(1===1);
+console.log('1'===1);
+
+console.log(1==1);
+console.log('1'==1);
+
+let age3=16;
+let type=age3>18? "adult tiket" : "child";
+console.log(type);
+
+let age4=24;
+let type2=age4>18? "adult tiket" : "child";
+console.log(type2);
+
+console.log(!false);
+
+console.log(false&&true);
+
+console.log(true||false);
+
+console.log(true&&true);
+
+
+
