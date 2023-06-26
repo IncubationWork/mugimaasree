9a78a961744ad20432d955eebb20cf3cfe69dcaa
mugimaasree <mugimaasree@gmail.com>
2023-06-26 12:19:13 +0530

    emp

--- employee.js
+++ employee.js
@@ -1,25 +1,28 @@
 class Person{
-    constructor(name,age,sex){
-        this.name=name;
-        this.age=age;
-        this.sex=sex;
-    }
+    constructor(name,age,sex)
+        {
+            this.name=name;
+            this.age=age;
+            this.sex=sex;
+        }
     getVoiceType(){
-        if(this.age>50 && this.sex==="male"){
-            return "hoarse";
-       }
-        else if (this.age<18 && this.sex==="male"){
-            return "soft";
-       }
-    }
-}
+            if(this.age>50 && this.sex==="male")
+                {
+                    return "hoarse";
+                }
+            else if (this.age<18 && this.sex==="male")
+                {
+                    return "soft";
+                }
+            }
+        }
 class Employee extends Person{
     constructor(name,age,sex,id)
-    {
-        super(name,age,sex)
-        this.id=id;
+        {
+            super(name,age,sex)
+            this.id=id;
+        }
     }
-}
 let a= new Employee("suthan",17,"male",1);
 let b= new Employee("mugunthan",51,"male",2);
 console.log(a.getVoiceType());
