39178e57768afed626a506960bdd8915dde2c346
mugimaasree <mugimaasree@gmail.com>
2023-08-22 12:53:57 +0530

    mvc

---
+++ angular/weather-app/src/app/location-filter-pipe.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { LocationFilterPipePipe } from './location-filter-pipe.pipe';
+
+describe('LocationFilterPipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new LocationFilterPipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
