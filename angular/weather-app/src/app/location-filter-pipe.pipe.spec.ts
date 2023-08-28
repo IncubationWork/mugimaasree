71e6bb1718fe52ae8ae7d27c31b64d70d15be0d1
mugimaasree <mugimaasree@gmail.com>
2023-08-28 09:33:52 +0530

    weather

--- angular/weather-app/src/app/location-filter-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LocationFilterPipePipe } from './location-filter-pipe.pipe';
-
-describe('LocationFilterPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new LocationFilterPipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- angular/weatherapp/src/app/app-routing.module.ts
+++ angular/weatherapp/src/app/app-routing.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { WetherForecastComponent } from './wether-forecast/wether-forecast.component';
-import { WeatherComponent } from './weather/weather.component';
-const routes: Routes = [ { path: '', redirectTo: 'weather', pathMatch: 'full' },
-{ path: 'weather', component: WeatherComponent },
-{ path: 'forecast', component: WetherForecastComponent },];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
